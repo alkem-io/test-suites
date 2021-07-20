4541b6750c2a28b49406904d4c6bbce902c910af
Evgeni Dimitrov <comoque@gmail.com>
2021-07-20 12:54:26 +0300

    Initial setup for UI e2e tests

---
+++ test/functional/e2e/login-page-object.ts
@@ -0,0 +1,60 @@
+import puppeteer from 'puppeteer';
+
+const usernameField = 'input[name=password_identifier]';
+const passwordField = 'input[name=password]';
+const signInButton = 'button[value=password]';
+const signInButtonHome = '.col a[href="/auth/login"] span';
+const authenticatedUserAvatar =
+  '#main .container-fluid div:nth-child(1).reversed span';
+const invalidCredentialsMessage = '.alert-danger';
+const userProfileButton = '.col span';
+const signOutButton = '.popover .MuiBox-root:nth-child(3) button';
+
+export default class LoginPage {
+  page: any;
+  value: any;
+
+  async invalidCredentials(page: puppeteer.Page) {
+    await page.waitForSelector(invalidCredentialsMessage);
+    let element = await page.$(invalidCredentialsMessage);
+    return (this.value = await page.evaluate(
+      (el: { textContent: any }) => el.textContent,
+      element
+    ));
+  }
+
+  async verifyAvailableAvatar(page: puppeteer.Page) {
+    await page.waitForSelector(authenticatedUserAvatar);
+    let element = await page.$(authenticatedUserAvatar);
+    return (this.value = await page.evaluate(
+      (el: { textContent: any }) => el.textContent,
+      element
+    ));
+  }
+
+  async login(page: any, username: any, password: any) {
+    await page.waitForSelector(usernameField);
+    await page.type(usernameField, username);
+    await page.type(passwordField, password);
+    await page.click(signInButton);
+  }
+
+  async clicksUserProfileButton(page: puppeteer.Page) {
+    await page.waitForSelector(userProfileButton);
+    await page.click(userProfileButton);
+  }
+
+  async clicksSignOut(page: puppeteer.Page) {
+    await page.waitForSelector(signOutButton);
+    await page.click(signOutButton);
+  }
+
+  async signInButtonHomeIsDisplayed(page: puppeteer.Page) {
+    await page.waitForSelector(signInButtonHome);
+    let element = await page.$(signInButtonHome);
+    return (this.value = await page.evaluate(
+      (el: { textContent: any }) => el.textContent,
+      element
+    ));
+  }
+}
---
+++ test/functional/e2e/registration-page-object.ts
@@ -0,0 +1,79 @@
+import puppeteer from 'puppeteer';
+
+const usernameField = "input[name='traits.email']";
+const passwordField = 'input[name=password]';
+const firstNameField = "input[name='traits.name.first']";
+const lastNameField = "input[name='traits.name.last']";
+const acceptTermsInput = "input[name='traits.accepted_terms']";
+const signInButton = 'button[value=password]';
+const authenticatedUserAvatar =
+  '#main .container-fluid div:nth-child(1).reversed span';
+const warningRiquiredFieldSignUp = '.alert-warning';
+
+export default class RegistrationPage {
+  page: any;
+  value: any;
+
+  async setUsername(page: any, username?: any) {
+    await page.waitForSelector(usernameField);
+    await page.type(usernameField, username);
+    await page.click(signInButton);
+    // await page.waitForSelector(warningRiquiredFieldSignUp);
+    // let element = await page.$(warningRiquiredFieldSignUp);
+    // this.value = await page.evaluate(
+    //   (el: { textContent: any }) => el.textContent,
+    //   element
+    // );
+  }
+
+  async verifyWirningRequiredSignInField(page: puppeteer.Page) {
+    // await page.waitForSelector(usernameField);
+    // await page.type(usernameField, username);
+    // await page.click(signInButton);
+    await page.waitForSelector(warningRiquiredFieldSignUp);
+    let element = await page.$(warningRiquiredFieldSignUp);
+    return (this.value = await page.evaluate(
+      (el: { textContent: any }) => el.textContent,
+      element
+    ));
+  }
+
+  async register(
+    page: any,
+    username?: any,
+    password?: any,
+    firstName?: string,
+    lastName?: string,
+    acceptTerms?: boolean
+  ) {
+    await page.waitForSelector(usernameField);
+    await page.type(usernameField, username);
+    await page.type(passwordField, password);
+    await page.type(firstNameField, firstName);
+    await page.type(lastNameField, lastName);
+    await page.click(acceptTermsInput, acceptTerms);
+    await page.click(signInButton);
+    await page.waitForSelector(authenticatedUserAvatar);
+    let element = await page.$(authenticatedUserAvatar);
+    this.value = await page.evaluate(
+      (el: { textContent: any }) => el.textContent,
+      element
+    );
+  }
+
+  async verifyAuthenticatedUserAvatar(page: puppeteer.Page) {
+    // await page.waitForSelector(usernameField);
+    // await page.type(usernameField, username);
+    // await page.type(passwordField, password);
+    // await page.type(firstNameField, firstName);
+    // await page.type(lastNameField, lastName);
+    // await page.click(acceptTermsInput, acceptTerms);
+    // await page.click(signInButton);
+    await page.waitForSelector(authenticatedUserAvatar);
+    let element = await page.$(authenticatedUserAvatar);
+    return (this.value = await page.evaluate(
+      (el: { textContent: any }) => el.textContent,
+      element
+    ));
+  }
+}
