24b9c2d6af0d75a33cb30793f41cef04766a4813
Evgeni Dimitrov <comoque@gmail.com>
2021-11-19 10:38:28 +0200

    Added example tests to test the script

---
+++ test/non-functional/auth/challenge-admin-simple-auth-test.it-spec.ts
@@ -0,0 +1,59 @@
+import {
+  assignChallengeAdmin,
+  removeUserAsChallengeAdmin,
+  userAsChallengeAdminVariablesData,
+} from '@test/utils/mutations/authorization-mutation';
+import {
+  createEcoverse,
+  ecoverseVariablesData,
+  uniqueId,
+} from '@test/utils/mutations/create-mutation';
+import {
+  updateEcoverse,
+  updateEcoverseVariablesData,
+} from '@test/utils/mutations/update-mutation';
+import { TestUser } from '@test/utils/token.helper';
+import { mutation } from '../../utils/graphql.request';
+import {
+  qaUserId,
+  challengeId,
+  hubId,
+  notAuthorizedCode,
+  dataNull,
+  forbiddenCode,
+  userNotRegistered,
+} from './common-auth-variables';
+
+describe('Challenge Admin - authorization test suite', () => {
+  beforeAll(async () => {
+    await mutation(
+      assignChallengeAdmin,
+      userAsChallengeAdminVariablesData(qaUserId, challengeId)
+    );
+  });
+
+  afterAll(async () => {
+    await mutation(
+      removeUserAsChallengeAdmin,
+      userAsChallengeAdminVariablesData(qaUserId, challengeId)
+    );
+  });
+
+  test.each`
+    mutations         | mut                 | variables                                                               | expected
+    ${createEcoverse} | ${'createEcoverse'} | ${ecoverseVariablesData('ecox-' + uniqueId, 'ecox-' + uniqueId, hubId)} | ${notAuthorizedCode}
+    ${updateEcoverse} | ${'updateEcoverse'} | ${updateEcoverseVariablesData(hubId, 'newnameCA')}                      | ${notAuthorizedCode}
+  `(
+    'Role challengeAdmin get: $expected, when run mutation: $mut',
+    async ({ mutations, variables, expected }) => {
+      const response = await mutation(mutations, variables, TestUser.QA_USER);
+
+      const responseData = JSON.stringify(response.body).replace('\\', '');
+      expect(response.status).toBe(200);
+      expect(response.body.data).toEqual(dataNull);
+      expect(responseData).not.toContain(expected);
+      expect(responseData).toContain(forbiddenCode);
+      expect(responseData).not.toContain(userNotRegistered);
+    }
+  );
+});
---
+++ test/non-functional/auth/ecoverse-admin-simple-auth-test.it-spec.ts
@@ -0,0 +1,71 @@
+import {
+  assignEcoverseAdmin,
+  removeUserAsEcoverseAdmin,
+  userAsEcoverseAdminVariablesData,
+} from '@test/utils/mutations/authorization-mutation';
+import {
+  createEcoverse,
+  ecoverseVariablesData,
+  uniqueId,
+} from '@test/utils/mutations/create-mutation';
+import {
+  updateEcoverse,
+  updateEcoverseVariablesData,
+} from '@test/utils/mutations/update-mutation';
+import { TestUser } from '@test/utils/token.helper';
+import { mutation } from '../../utils/graphql.request';
+import {
+  qaUserId,
+  hubId,
+  notAuthorizedCode,
+  dataNull,
+  forbiddenCode,
+} from './common-auth-variables';
+
+describe('Ecoverse Admin - authorization test suite', () => {
+  beforeAll(async () => {
+    await mutation(
+      assignEcoverseAdmin,
+      userAsEcoverseAdminVariablesData(qaUserId, hubId)
+    );
+  });
+
+  afterAll(async () => {
+    await mutation(
+      removeUserAsEcoverseAdmin,
+      userAsEcoverseAdminVariablesData(qaUserId, hubId)
+    );
+  });
+
+  test.each`
+    mutations         | mut                 | variables
+    ${updateEcoverse} | ${'updateEcoverse'} | ${updateEcoverseVariablesData(hubId, 'newnameEA')}
+  `(
+    'Role ecoverseAdmin get: $expectedOne, when run mutation: $mut',
+    async ({ mutations, variables }) => {
+      const response = await mutation(mutations, variables, TestUser.QA_USER);
+
+      const responseData = JSON.stringify(response.body).replace('\\', '');
+      expect(response.status).toBe(200);
+      expect(response.body.data).not.toEqual(dataNull);
+      expect(responseData).not.toContain(forbiddenCode);
+      expect(responseData).not.toContain(notAuthorizedCode);
+    }
+  );
+
+  test.each`
+    mutations         | mut                 | variables
+    ${createEcoverse} | ${'createEcoverse'} | ${ecoverseVariablesData('ecoxx-' + uniqueId, 'ecoxx-' + uniqueId, hubId)}
+  `(
+    "Role ecoverseAdmin don't get: forbiddenCode, when run mutation: $mut",
+    async ({ mutations, variables }) => {
+      const response = await mutation(mutations, variables, TestUser.QA_USER);
+
+      const responseData = JSON.stringify(response.body).replace('\\', '');
+      expect(response.status).toBe(200);
+      expect(response.body.data).toEqual(dataNull);
+      expect(responseData).toContain(forbiddenCode);
+      expect(responseData).not.toContain(notAuthorizedCode);
+    }
+  );
+});
