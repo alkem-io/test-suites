1c85a656ed1c761e7ae94abea9e282c73d870a3e
Svetoslav Petkov <svetoslav@alkem.io>
2022-04-04 11:36:42 +0300

    running register flows in parallel brings 3x lower waiting times

--- test/testSetup.ts
+++ test/testSetup.ts
@@ -15,38 +15,42 @@ module.exports = async () => {
   const userNames = Object.values(TestUser).filter(
     x => x !== TestUser.GLOBAL_ADMIN
   );
-  for (const userName of userNames) {
-    const [firstName, lastName] = getUserName(userName);
-    const email = `${userName}@alkem.io`;
-
-    try {
-      await registerInKratosOrFail(firstName, lastName, email);
-    } catch (e) {
-      const err = e as Error;
-      if (err.message.indexOf('exists already') > -1) {
-        console.warn(`User ${email} already registered in Kratos`);
-      } else {
-        throw new Error(err.message);
-      }
-    }
-    console.info(`User ${email} registered in Kratos`);
-    await verifyInKratosOrFail(email);
-    console.info(`User ${email} verified`);
-    try {
-      await registerInAlkemioOrFail(firstName, lastName, email);
-    } catch (e) {
-      const err = e as Error;
-      if (err.message.indexOf('User already exists') > -1) {
-        console.warn(`User ${email} already registered in Alkemio`);
-      } else {
-        throw new Error(err.message);
-      }
-    }
-    console.info(`User ${email} registered in Alkemio`);
-  }
+  // run register flows in parallel
+  // 3x less waiting time
+  await Promise.allSettled(userNames.map(userRegisterFlow));
 };
 
 const getUserName = (userName: string): [string, string] => {
   const [first, last] = userName.split('.');
   return [first, last];
+};
+
+const userRegisterFlow = async (userName: string) => {
+  const [firstName, lastName] = getUserName(userName);
+  const email = `${userName}@alkem.io`;
+
+  try {
+    await registerInKratosOrFail(firstName, lastName, email);
+  } catch (e) {
+    const err = e as Error;
+    if (err.message.indexOf('exists already') > -1) {
+      console.warn(`User ${email} already registered in Kratos`);
+    } else {
+      throw new Error(err.message);
+    }
+  }
+  console.info(`User ${email} registered in Kratos`);
+  await verifyInKratosOrFail(email);
+  console.info(`User ${email} verified`);
+  try {
+    await registerInAlkemioOrFail(firstName, lastName, email);
+  } catch (e) {
+    const err = e as Error;
+    if (err.message.indexOf('User already exists') > -1) {
+      console.warn(`User ${email} already registered in Alkemio`);
+    } else {
+      throw new Error(err.message);
+    }
+  }
+  console.info(`User ${email} registered in Alkemio`);
 };
