f5027408e230874a4484a45acff13eaf4423f1e2
Aleksandar Stojanovic <aleksandarstojanovic.coa@gmail.com>
2022-07-11 16:52:25 +0200

    Wip preference setup.

--- test/functional-api/zcommunications/notifications/notifications-opportunity-collaboration.it-spec.ts
+++ test/functional-api/zcommunications/notifications/notifications-opportunity-collaboration.it-spec.ts
@@ -2,6 +2,8 @@ import { mutation } from '@test/utils/graphql.request';
 import {
   UserPreferenceType,
   changePreferenceUser,
+  changePreferenceHub,
+  HubPreferenceType,
 } from '@test/utils/mutations/preferences-mutation';
 import { uniqueId } from '@test/utils/mutations/create-mutation';
 import { deleteMailSlurperMails } from '@test/utils/mailslurper.rest.requests';
@@ -45,11 +47,19 @@ const subjectAdmin = 'New user is interested to collaborate on opportunity';
 const subjectUser = 'Your interest to collaborate was received!';
 const preferencesConfig = [
   {
-    userID: users.qaUserId,
+    userID: users.qaUserEmail,
     type: UserPreferenceType.INTERESTED_IN_COLLABORATION_ADMIN,
   },
   {
-    userID: users.hubMemberId,
+    userID: users.hubMemberEmail,
+    type: UserPreferenceType.INTERESTED_IN_COLLABORATION_USER,
+  },
+  {
+    userID: users.nonHubMemberEmail,
+    type: UserPreferenceType.INTERESTED_IN_COLLABORATION_USER,
+  },
+  {
+    userID: users.hubAdminEmail,
     type: UserPreferenceType.INTERESTED_IN_COLLABORATION_USER,
   },
 ];
@@ -67,7 +77,6 @@ beforeAll(async () => {
   }
 
   await deleteMailSlurperMails();
-
   // Hub:
   //  Members:users.hubAdminId, users.hubMemberId, users.qaUserId
   //  Admins: users.hubAdminId
@@ -101,6 +110,10 @@ beforeAll(async () => {
   );
 });
 
+beforeEach(async () => {
+  await deleteMailSlurperMails();
+});
+
 afterAll(async () => {
   await removeOpportunity(entitiesId.opportunityId);
   await removeChallenge(entitiesId.challengeId);
@@ -110,6 +123,11 @@ afterAll(async () => {
 
 afterEach(async () => {
   await removeRelation(relationId);
+  await changePreferenceHub(
+    entitiesId.hubId,
+    HubPreferenceType.ANONYMOUS_READ_ACCESS,
+    'false'
+  );
 });
 
 describe('Preferences enabled for Admin and User interested', () => {
@@ -119,11 +137,39 @@ describe('Preferences enabled for Admin and User interested', () => {
     }
   });
 
-  beforeEach(async () => {
-    await deleteMailSlurperMails();
+  test('User member of a Hub registers interest in collaboration on Opp (child of Challenge / child of Hub)- Opp admin gets notification, User gets notification', async () => {
+    // Act
+    const createRelationResponse = await createRelation(
+      entitiesId.opportunityId,
+      relationType,
+      relationDescription,
+      relationActorName,
+      relationActorType,
+      relationActorRole,
+      TestUser.HUB_ADMIN
+    );
+
+    relationId = createRelationResponse.body.data.createRelation.id;
+    await delay(4000);
+
+    const getEmailsData = await getMailsData();
+    // Assert
+    expect(getEmailsData[1]).toEqual(2);
+    expect(getEmailsData[0]).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          subject: subjectAdmin,
+          toAddresses: [users.qaUserEmail],
+        }),
+        expect.objectContaining({
+          subject: subjectUser,
+          toAddresses: [users.hubAdminEmail],
+        }),
+      ])
+    );
   });
 
-  test.only('User member of a Hub registers interest in collaboration on Opp (child of Challenge / child of Hub)- Opp admin gets notification, User gets notification', async () => {
+  test('User member of a Challenge registers interest in collaboration on Opp - Opp admin gets notification, User gets notification', async () => {
     // Act
     const createRelationResponse = await createRelation(
       entitiesId.opportunityId,
@@ -155,9 +201,79 @@ describe('Preferences enabled for Admin and User interested', () => {
     );
   });
 
-  // test('User member of a Challenge registers interest in collaboration on Opp - Opp admin gets notification, User gets notification', async () => {});
+  test('User NOT member of a Hub register interest in collaboration on Opp (child of Challenge / child of Hub) - Opp admin get notification, User gets notification', async () => {
+    await changePreferenceHub(
+      entitiesId.hubId,
+      HubPreferenceType.ANONYMOUS_READ_ACCESS,
+      'true'
+    );
+    // Act
+    const createRelationResponse = await createRelation(
+      entitiesId.opportunityId,
+      relationType,
+      relationDescription,
+      relationActorName,
+      relationActorType,
+      relationActorRole,
+      TestUser.NON_HUB_MEMBER
+    );
 
-  // test('no notification when Non-hub member cannot join a Hub - GA, EA and Joiner', async () => {});
+    relationId = createRelationResponse.body.data.createRelation.id;
+    await delay(4000);
+
+    const getEmailsData = await getMailsData();
+    // Assert
+    expect(getEmailsData[1]).toEqual(2);
+    expect(getEmailsData[0]).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          subject: subjectAdmin,
+          toAddresses: [users.qaUserEmail],
+        }),
+        expect.objectContaining({
+          subject: subjectUser,
+          toAddresses: [users.nonHubMemberEmail],
+        }),
+      ])
+    );
+  });
 });
 
-// describe('Preferences disabled for Community Admin and User interested', () => {});
+describe.only('Preferences disabled for Community Admin and User interested', () => {
+  // beforeAll(async () => {
+  //   for (const config of preferencesConfig) {
+  //     await changePreferenceUser(config.userID, config.type, 'false');
+  //   }
+  // });
+  // beforeEach(async () => {
+  //   await deleteMailSlurperMails();
+  // });
+  test('User member of a Challenge registers interest in collaboration on Opp - no one gets notifications', async () => {
+    for (const config of preferencesConfig) {
+      const res = await changePreferenceUser(
+        config.userID,
+        config.type,
+        'false'
+      );
+      console.log(res.body);
+    }
+
+    // Act
+    const createRelationResponse = await createRelation(
+      entitiesId.opportunityId,
+      relationType,
+      relationDescription,
+      relationActorName,
+      relationActorType,
+      relationActorRole,
+      TestUser.HUB_MEMBER
+    );
+
+    relationId = createRelationResponse.body.data.createRelation.id;
+    await delay(10000);
+
+    const getEmailsData = await getMailsData();
+    // Assert
+    expect(getEmailsData[1]).toEqual(0);
+  });
+});
