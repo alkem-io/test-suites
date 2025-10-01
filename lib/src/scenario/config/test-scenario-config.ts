5eec715986bd83ee70cc156be5497327f375f1ad
Svetoslav <svetoslav@alkem.io>
2025-10-01 10:24:25 +0300

    fixed failing import

--- lib/src/scenario/config/test-scenario-config.ts
+++ lib/src/scenario/config/test-scenario-config.ts
@@ -1,4 +1,5 @@
-import { SpacePrivacyMode, CommunityMembershipPolicy } from "@src/core/generated/alkemio-schema";
+// import { SpacePrivacyMode, CommunityMembershipPolicy } from "@src/core/generated/alkemio-schema";
+import { SpacePrivacyMode, CommunityMembershipPolicy } from "../../../../lib/src/core/generated/alkemio-schema";
 import { TestUser } from "../../common/enums/test.user";
 
 export interface TestScenarioConfig {
