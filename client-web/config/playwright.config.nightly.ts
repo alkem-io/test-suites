95820905f25ed3fcb55e530db9dc8cc0a73bf177
Svetoslav <svetoslav@alkem.io>
2025-12-15 16:45:40 +0200

    fullyParallel: false

--- client-web/config/playwright.config.nightly.ts
+++ client-web/config/playwright.config.nightly.ts
@@ -51,7 +51,7 @@ export default defineConfig({
   testMatch: testPattern,
 
   /* Run tests in files in parallel */
-  fullyParallel: true,
+  fullyParallel: false,
 
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: true,
