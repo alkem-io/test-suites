5cbc207bb888abf46263441538a62ccb156d0262
Svetoslav <svetoslav@alkem.io>
2025-12-16 14:58:31 +0200

    Update Playwright configuration for improved tracing and video handling

--- client-web/config/playwright.config.nightly.ts
+++ client-web/config/playwright.config.nightly.ts
@@ -75,18 +75,16 @@ export default defineConfig({
 
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
+    headless: true,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-all-retries',
-    headless: true,
 
     /* Screenshot on failure */
     screenshot: 'only-on-failure',
 
     /* Video on retry */
-    video: 'on-first-retry',
+    video: 'retain-on-failure',
   },
 
   timeout: 30000,
