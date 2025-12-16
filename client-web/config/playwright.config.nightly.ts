4a8ddeb9db3650644e395c860af875878fd71276
Svetoslav <svetoslav@alkem.io>
2025-12-16 10:01:55 +0200

    set workers to 1 in nightly Playwright configuration

--- client-web/config/playwright.config.nightly.ts
+++ client-web/config/playwright.config.nightly.ts
@@ -65,7 +65,7 @@ export default defineConfig({
 
   // % of the available CPUs
   // workers: '100%',
-  workers: '100%',
+  workers: 1,
 
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
